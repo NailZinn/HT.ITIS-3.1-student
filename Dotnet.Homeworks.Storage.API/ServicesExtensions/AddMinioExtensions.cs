bd29358bf8a5546cae26207371bbd6def2598d22
NailZinn <nailzinnurov17@gmail.com>
2023-12-06 17:42:35 +0300

    added hw8

--- Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
+++ Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
@@ -1,12 +1,19 @@
-using Dotnet.Homeworks.Storage.API.Configuration;
+using Minio;
+using MinioConfig = Dotnet.Homeworks.Storage.API.Configuration.MinioConfig;
 
 namespace Dotnet.Homeworks.Storage.API.ServicesExtensions;
 
 public static class AddMinioExtensions
 {
-    public static IServiceCollection AddMinioClient(this IServiceCollection services,
-        MinioConfig minioConfiguration)
+    public static IServiceCollection AddMinioClient(this IServiceCollection services, MinioConfig minioConfiguration)
     {
-        throw new NotImplementedException();
+        services.AddMinio(client =>
+        {
+            client.WithSSL(minioConfiguration.WithSsl);
+            client.WithEndpoint(minioConfiguration.Endpoint, minioConfiguration.Port);
+            client.WithCredentials(minioConfiguration.Username, minioConfiguration.Password);
+        });
+        
+        return services;
     }
 }
